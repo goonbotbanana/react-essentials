d07b0b20c1f898fa2cbecf6a52feb88571a79a86
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-02-12 12:57:15 -0500

    replace app header element with imported header component

--- section-3/src/App.jsx
+++ section-3/src/App.jsx
@@ -1,14 +1,11 @@
+import Header from "./components/Header.jsx";
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      {/* <Header />
+      or write like this: */}
+      <Header></Header>
       <main>
         <h2>Time to get started!</h2>
       </main>
