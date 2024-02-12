636f487cc0c6db36d27388a01cd01bceb3bf3edd
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-02-12 13:45:25 -0500

    set image as imported dynamic value

--- section-3/src/components/Header.jsx
+++ section-3/src/components/Header.jsx
@@ -1,3 +1,5 @@
+import reactImg from "../assets/react-core-concepts.png";
+
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
@@ -11,10 +13,10 @@ function Header() {
   // this makes the jsx code leaner, best practice
 
   const desc = reactDescriptions[genRandomInt(2)];
-  
+
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       {/* <p>
         {reactDescriptions[genRandomInt(2)]} React concepts you will need for
