00f3015ebbfa59504207bbcdd6badf185db580e5
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-02-12 13:17:25 -0500

    add output for dynamic values in header

--- section-3/src/components/Header.jsx
+++ section-3/src/components/Header.jsx
@@ -1,14 +1,31 @@
-function Header() {
-    return (
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-    );
-  }
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
-  export default Header;
+function genRandomInt(max) {
+  let num = Math.floor(Math.random() * (max + 1)); // if max = 3, this returns a number 0-3
+  console.log(`Integer generated: ${num}`);
+  return num;
+}
+
+function Header() {
+  // alternatively, a const can be made outside the return statement to hold the generated int
+  // this makes the jsx code leaner, best practice
+
+  const desc = reactDescriptions[genRandomInt(2)];
+  
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      {/* <p>
+        {reactDescriptions[genRandomInt(2)]} React concepts you will need for
+        almost any app you are going to build!
+      </p> */}
+      <p>
+        {desc} React concepts you will need for almost any app you are going to
+        build!
+      </p>
+    </header>
+  );
+}
+
+export default Header;
