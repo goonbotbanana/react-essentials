69bd99b112023ac6dfccb6495bf2dc698885dc07
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-02-19 23:55:54 -0500

    add conditional for selectedTopic

--- section-3/src/App.jsx
+++ section-3/src/App.jsx
@@ -6,7 +6,7 @@ import { EXAMPLES } from "./data.js";
 import { useState } from "react";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("components"); //destructured
+  const [selectedTopic, setSelectedTopic] = useState(); //destructured
   // initial value = Please click a button
   // selectedTopic = current state value
   // setSelectedTopic = state updating function, updates stored value and tells react to re-execute the component
@@ -50,13 +50,18 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
+          {/* if there is no selected topic, render the p, otherwise, render tab content */}
+          {!selectedTopic ? (
+            <p>Please select a topic.</p>
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
         </section>
       </main>
     </div>
