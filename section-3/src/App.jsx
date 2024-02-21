8295cd83270ea1450f0f84fe518eda25c600201a
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-02-21 14:50:02 -0500

    add dynamic output for list of core concepts

--- section-3/src/App.jsx
+++ section-3/src/App.jsx
@@ -46,11 +46,16 @@ function App() {
               image={CORE_CONCEPTS[0].image}
             /> */}
 
-            <CoreConcept {...CORE_CONCEPTS[0]} />
+            {/* <CoreConcept {...CORE_CONCEPTS[0]} /> */}
             {/* spread operator pulls out all key value pairs from the object at index 0*/}
-            <CoreConcept {...CORE_CONCEPTS[1]} />
+            {/* <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} /> */}
+
+            {/* map produces new array based on existing array, and executes once for every item in the array */}
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept {...conceptItem} />
+            ))}
           </ul>
         </section>
         <section id="examples">
